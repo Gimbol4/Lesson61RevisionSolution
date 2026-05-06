b33e8f6fec068671cb398c10f29268cd585cfabb
Gimbol4 <Shishkanov.yd@gmail.com>
2026-05-06 17:23:20 +0300

    solve 3 task

--- Task03/logic.cpp
+++ Task03/logic.cpp
@@ -4,3 +4,27 @@
 // Дан вектор целочисленных значений. Необходимо разработать эффективный алгоритм
 // и реализовать функцию, которая подсчитывает количество локальных минимумов 
 // и максимумов.
+
+#include "logic.h"
+
+int count_local_extreme_numbers(int* array, int size) {
+	int count = 0;
+	if (size <= 1) {
+		return 0;
+	}
+	if ((array[0] > array[1]) || (array[0] < array[1])) {
+		count++;
+	}
+
+	if ((array[size - 1] > array[size - 2]) || (array[size - 1] < array[size - 2])) {
+		count++;
+	}
+
+	for (int i = 1; i < size - 1; i++)
+	{
+		if ((array[i] > array[i - 1] && array[i] > array[i + 1]) || (array[i] < array[i - 1] && array[i] < array[i + 1])) {
+			count++;
+		}
+	}
+	return count;
+}
