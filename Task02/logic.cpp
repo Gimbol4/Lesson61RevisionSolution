5eb083117d1b821bf02676aa08312e1245460146
Gimbol4 <Shishkanov.yd@gmail.com>
2026-05-06 17:02:24 +0300

    solve 2 task

--- Task02/logic.cpp
+++ Task02/logic.cpp
@@ -4,3 +4,23 @@
 // Дан вектор целочисленных значений. Необходимо разработать эффективный алгоритм
 // и реализовать функцию, которая вычисляет среднее арифметическое ненулевых 
 // элементов вектора.
+
+double calculate_arithmetical_mean_of_nonzero_elements(int* array, int size) {
+	if (size <= 0) {
+		return 0.0;
+	}
+	double summ = 0;
+	int count = 0;
+
+	for (int i = 0; i < size; i++)
+	{
+		if (*(array + i) != 0) {
+			summ += *(array + i);
+			count++;
+		}
+	}
+	if (summ == 0) {
+		return 0.0;
+	}
+	return summ / count;
+}
