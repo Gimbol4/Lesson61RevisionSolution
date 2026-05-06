442c6c1988913345a9b3629053598114e4ca8f41
Gimbol4 <Shishkanov.yd@gmail.com>
2026-05-06 18:13:23 +0300

    solve 6 task

--- Task06/logic.cpp
+++ Task06/logic.cpp
@@ -5,3 +5,34 @@
 // и реализовать функцию, которая сортирует часть элементов заданного вектора
 // в указанном диапазоне порядковых номеров либо по возрастанию, 
 // либо по убыванию.
+#include "logic.h"
+
+void sort_in_range(int* array, int size, int start, int end, bool type) {
+    if (size <= 0) return;
+
+    if (start < 0 || start >= size || end < 0 || end >= size) return;
+
+    if (start > end) {
+        int temp = start;
+        start = end;
+        end = temp;
+    }
+
+    for (int i = 0; i < (end - start); i++) {
+        for (int j = start; j < end - i; j++) {
+            bool needSwap = false;
+            if (type) {
+                if (array[j] > array[j + 1]) needSwap = true;
+            }
+            else {
+                if (array[j] < array[j + 1]) needSwap = true;
+            }
+
+            if (needSwap) {
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
+        }
+    }
+}
